1c669695d480bf1509e06ea9d4fcaece7aabaeac
Anton Kapralov <a.e.kapralov@gmail.com>
2019-03-06 07:46:18 -0800

    Initial commit with Spring Initializer generated code.

---
+++ currency-trends-monitor-server/src/main/java/kae/demo/currencytrendsmonitorserver/CurrencyTrendsMonitorServerApplication.java
@@ -0,0 +1,12 @@
+package kae.demo.currencytrendsmonitorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyTrendsMonitorServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CurrencyTrendsMonitorServerApplication.class, args);
+  }
+}
