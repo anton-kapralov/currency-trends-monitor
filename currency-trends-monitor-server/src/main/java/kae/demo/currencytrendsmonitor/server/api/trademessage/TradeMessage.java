29d25011fe8117a4a8346f217b5c6376ca50e365
Anton Kapralov <a.e.kapralov@gmail.com>
2019-03-13 19:31:24 -0700

    Remove Lombok.

--- currency-trends-monitor-server/src/main/java/kae/demo/currencytrendsmonitor/server/api/trademessage/TradeMessage.java
+++ currency-trends-monitor-server/src/main/java/kae/demo/currencytrendsmonitor/server/api/trademessage/TradeMessage.java
@@ -2,12 +2,8 @@ package kae.demo.currencytrendsmonitor.server.api.trademessage;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import lombok.Builder;
-import lombok.Data;
 
 /** */
-@Data
-@Builder
 public final class TradeMessage {
 
   private String userId;
@@ -25,4 +21,68 @@ public final class TradeMessage {
   private LocalDateTime timePlaced;
 
   private String originatingCountry;
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+  public String getCurrencyFrom() {
+    return currencyFrom;
+  }
+
+  public void setCurrencyFrom(String currencyFrom) {
+    this.currencyFrom = currencyFrom;
+  }
+
+  public String getCurrencyTo() {
+    return currencyTo;
+  }
+
+  public void setCurrencyTo(String currencyTo) {
+    this.currencyTo = currencyTo;
+  }
+
+  public BigDecimal getAmountSell() {
+    return amountSell;
+  }
+
+  public void setAmountSell(BigDecimal amountSell) {
+    this.amountSell = amountSell;
+  }
+
+  public BigDecimal getAmountBuy() {
+    return amountBuy;
+  }
+
+  public void setAmountBuy(BigDecimal amountBuy) {
+    this.amountBuy = amountBuy;
+  }
+
+  public BigDecimal getRate() {
+    return rate;
+  }
+
+  public void setRate(BigDecimal rate) {
+    this.rate = rate;
+  }
+
+  public LocalDateTime getTimePlaced() {
+    return timePlaced;
+  }
+
+  public void setTimePlaced(LocalDateTime timePlaced) {
+    this.timePlaced = timePlaced;
+  }
+
+  public String getOriginatingCountry() {
+    return originatingCountry;
+  }
+
+  public void setOriginatingCountry(String originatingCountry) {
+    this.originatingCountry = originatingCountry;
+  }
 }
